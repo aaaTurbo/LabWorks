c9b4105f0243fa6b30c21ea49b8f5c1b7d45c07f
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-09-22 10:12:30 +0300

    Update FirstLab.java

--- FirstLab.java
+++ FirstLab.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void randFillingArr(float[] arr, int min, int max){
         for (int i=0; i< arr.length; i++){
-            arr[i]=((float)(Math.random() *(2*max)) -min);
+            arr[i]=((float)(Math.random() *(2*max)) +min);
         }
     }
 
@@ -15,8 +15,8 @@ public class Main {
     }
 
     public static void fillingDoubleArr(double[][] a, float[] c, int[] b){
-        for (int i=0; i<7; i++){
-            for (int j = 0; j<14; j++){
+        for (int i=0; i<a.length; i++){
+            for (int j = 0; j<a[i].length; j++){
                 if (b[i] == 12) a[i][j] = Math.cbrt(Math.asin(Math.sin(c[j])));
                 else if (b[i] == 6 | b[i] == 8 | b[i] == 10) a[i][j] = Math.pow((Math.log10(Math.pow(Math.sin(c[j]), 2))), (3/Math.sin(c[j]) * (Math.pow(c[j]/2,3))+4));
                 else a[i][j]=Math.cbrt(Math.sin(Math.cbrt(c[j]/2)));
@@ -25,8 +25,8 @@ public class Main {
     }
 
     public static void printDoubleArr(double[][] a){
-        for(int i=0; i<7; i++){
-            for (int j=0; j<14; j++){
+        for(int i=0; i<a.length; i++){
+            for (int j=0; j<a[i].length; j++){
                 System.out.printf("%5.2f", a[i][j]);
                 System.out.print(" ");
             }
