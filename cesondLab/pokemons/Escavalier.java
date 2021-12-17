909cd3a4f99675ef09b8cb617b5e0596bbf8dc44
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:13:03 +0300

    Update Escavalier.java

--- cesondLab/pokemons/Escavalier.java
+++ cesondLab/pokemons/Escavalier.java
@@ -1,16 +1,19 @@
 package pokemons;
 
-import moves.*;
-import ru.ifmo.se.pokemon.*;
+import moves.DoubleTeam;
+import moves.Peck;
+import moves.Twineedle;
+import moves.XScissor;
+import ru.ifmo.se.pokemon.Type;
 
-public class Escavalier extends Karrablast{
-    public Escavalier(String name, int level){
+public class Escavalier extends Karrablast {
+    public Escavalier(String name, int level) {
         super(name, level);
-        setStats(70.0, 135.0, 105.0, 60.0, 105.0, 20.0);
-        setType(Type.BUG, Type.STEEL);
-        addMove(new XScissor());
-        addMove(new DoubleTeam());
-        addMove(new Peck());
-        addMove(new Twineedle());
+        this.setStats(70.0D, 135.0D, 105.0D, 60.0D, 105.0D, 20.0D);
+        this.setType(new Type[]{Type.BUG, Type.STEEL});
+        this.addMove(new XScissor());
+        this.addMove(new DoubleTeam());
+        this.addMove(new Peck());
+        this.addMove(new Twineedle());
     }
 }
