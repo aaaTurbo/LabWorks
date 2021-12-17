64c5b1e8f67ff4d1c0f72e71743765cd4eefec91
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:14:25 +0300

    Update Kirilia.java

--- cesondLab/pokemons/Kirilia.java
+++ cesondLab/pokemons/Kirilia.java
@@ -3,17 +3,15 @@ package pokemons;
 import moves.Charm;
 import moves.DoubleTeam;
 import moves.ThunderWave;
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.Type;
 
-public class Kirilia extends Ralts{
-
-    public Kirilia(String name, int level){
+public class Kirilia extends Ralts {
+    public Kirilia(String name, int level) {
         super(name, level);
-        setStats(38.0, 35.0, 35.0, 65.0, 55.0, 50.0);
-        setType(Type.FAIRY, Type.PSYCHIC);
-        addMove(new DoubleTeam());
-        addMove(new ThunderWave());
-        addMove(new Charm());
+        this.setStats(38.0D, 35.0D, 35.0D, 65.0D, 55.0D, 50.0D);
+        this.setType(new Type[]{Type.FAIRY, Type.PSYCHIC});
+        this.addMove(new DoubleTeam());
+        this.addMove(new ThunderWave());
+        this.addMove(new Charm());
     }
-
 }
