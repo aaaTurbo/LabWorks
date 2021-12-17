070593c19fc82bb8cd38afc558ee54e33a4a5162
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:08:37 +0300

    Update Pound.java

--- cesondLab/moves/Pound.java
+++ cesondLab/moves/Pound.java
@@ -1,19 +1,19 @@
 package moves;
 
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.PhysicalMove;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
 
 public class Pound extends PhysicalMove {
-    public Pound(){
-        super(Type.NORMAL, 40.0, 100.0);
+    public Pound() {
+        super(Type.NORMAL, 40.0D, 100.0D);
     }
 
-    @Override
     protected void applyOppDamage(Pokemon p, double damage) {
         super.applyOppDamage(p, damage);
     }
 
-    @Override
-    protected String describe(){
+    protected String describe() {
         return "кастует Pound";
     }
 }
