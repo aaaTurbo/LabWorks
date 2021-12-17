5e6a6f28b85b12cb7c850d02c8e20ca162cfb27d
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:15:23 +0300

    Update Ralts.java

--- cesondLab/pokemons/Ralts.java
+++ cesondLab/pokemons/Ralts.java
@@ -3,16 +3,14 @@ package pokemons;
 import moves.DoubleTeam;
 import moves.ThunderWave;
 import ru.ifmo.se.pokemon.Pokemon;
-import ru.ifmo.se.pokemon.StatusMove;
 import ru.ifmo.se.pokemon.Type;
 
 public class Ralts extends Pokemon {
-
-    public Ralts(String name, int level){
+    public Ralts(String name, int level) {
         super(name, level);
-        setStats(28.0, 25.0, 25.0, 45.0, 35.0, 40.0);
-        setType(Type.FAIRY, Type.PSYCHIC);
-        addMove(new DoubleTeam());
-        addMove(new ThunderWave());
+        this.setStats(28.0D, 25.0D, 25.0D, 45.0D, 35.0D, 40.0D);
+        this.setType(new Type[]{Type.FAIRY, Type.PSYCHIC});
+        this.addMove(new DoubleTeam());
+        this.addMove(new ThunderWave());
     }
 }
