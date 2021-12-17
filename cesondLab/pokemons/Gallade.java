7cdcd65aefcffd27bfc851f3ef32866ddc95b06e
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:13:31 +0300

    Update Gallade.java

--- cesondLab/pokemons/Gallade.java
+++ cesondLab/pokemons/Gallade.java
@@ -6,14 +6,14 @@ import moves.FocusBlust;
 import moves.ThunderWave;
 import ru.ifmo.se.pokemon.Type;
 
-public class Gallade extends Kirilia{
-    public Gallade(String name, int level){
+public class Gallade extends Kirilia {
+    public Gallade(String name, int level) {
         super(name, level);
-        setStats(68.0, 125.0, 65.0, 65.0, 115.0, 80.0);
-        setType(Type.FIGHTING, Type.PSYCHIC);
-        addMove(new DoubleTeam());
-        addMove(new ThunderWave());
-        addMove(new Charm());
-        addMove(new FocusBlust());
+        this.setStats(68.0D, 125.0D, 65.0D, 65.0D, 115.0D, 80.0D);
+        this.setType(new Type[]{Type.FIGHTING, Type.PSYCHIC});
+        this.addMove(new DoubleTeam());
+        this.addMove(new ThunderWave());
+        this.addMove(new Charm());
+        this.addMove(new FocusBlust());
     }
 }
