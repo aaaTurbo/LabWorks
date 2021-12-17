4560332ac87bac239018d74bc33306db35f088df
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:11:14 +0300

    Update Teeterdance.java

--- cesondLab/moves/Teeterdance.java
+++ cesondLab/moves/Teeterdance.java
@@ -1,28 +1,32 @@
 package moves;
 
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.Effect;
+import ru.ifmo.se.pokemon.PhysicalMove;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
+import ru.ifmo.se.pokemon.Type;
 
-public class TeeterDance extends PhysicalMove{
-    public TeeterDance(){super(Type.NORMAL, 0.0, 100.00);}
+public class TeeterDance extends PhysicalMove {
+    public TeeterDance() {
+        super(Type.NORMAL, 0.0D, 100.0D);
+    }
 
-    @Override
     protected void applyOppEffects(Pokemon p) {
         Effect.confuse(p);
     }
 
-    @Override
-    protected void applyOppDamage(Pokemon p, double damage){
-        int luck = ((int) (Math.random() * 10) + 1);
-        int turn = ((int) (Math.random() * 4) + 1);
-        int attack = (int) p.getStat(Stat.ATTACK);
-        if ((luck == 1) || (luck == 2) || (luck == 3)) {
-            for (int i=0; i < turn; i++) {
-                super.applyOppDamage(p, attack/turn);
+    protected void applyOppDamage(Pokemon p, double damage) {
+        int luck = (int)(Math.random() * 10.0D) + 1;
+        int turn = (int)(Math.random() * 4.0D) + 1;
+        int attack = (int)p.getStat(Stat.ATTACK);
+        if (luck == 1 || luck == 2 || luck == 3) {
+            for(int i = 0; i < turn; ++i) {
+                super.applyOppDamage(p, (double)(attack / turn));
             }
         }
+
     }
 
-    @Override
     protected String describe() {
         return "кастует Teeter Dance";
     }
