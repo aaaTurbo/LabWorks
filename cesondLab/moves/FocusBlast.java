57b92baf8c7c7c71ae96ffedff6bdbc0e35c9b24
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:07:36 +0300

    Update FocusBlast.java

--- cesondLab/moves/FocusBlast.java
+++ cesondLab/moves/FocusBlast.java
@@ -1,28 +1,28 @@
 package moves;
 
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.PhysicalMove;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
+import ru.ifmo.se.pokemon.Type;
 
 public class FocusBlust extends PhysicalMove {
-    public FocusBlust(){
-        super(Type.FIGHTING, 170.0, 70.0);
+    public FocusBlust() {
+        super(Type.FIGHTING, 170.0D, 70.0D);
     }
 
-    @Override
     protected void applyOppEffects(Pokemon p) {
-        int luck = ((int)(Math.random() *10) +0);
-        if (luck == 1){
+        int luck = (int)(Math.random() * 10.0D) + 0;
+        if (luck == 1) {
             p.setMod(Stat.SPECIAL_DEFENSE, -1);
         }
+
     }
 
-    @Override
     protected void applyOppDamage(Pokemon p, double damage) {
         super.applyOppDamage(p, damage);
     }
 
-    @Override
-    protected String describe(){
+    protected String describe() {
         return "кастует Focus Blast";
     }
-
 }
