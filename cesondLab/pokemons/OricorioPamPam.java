59833ce741d8177195a0b738f486e0258e5b6c14
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:14:49 +0300

    Update OricorioPamPam.java

--- cesondLab/pokemons/OricorioPamPam.java
+++ cesondLab/pokemons/OricorioPamPam.java
@@ -1,16 +1,20 @@
 package pokemons;
 
-import moves.*;
-import ru.ifmo.se.pokemon.*;
+import moves.Pound;
+import moves.Rest;
+import moves.Swagger;
+import moves.ThunderWave;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Type;
 
 public class OricorioPomPom extends Pokemon {
-    public OricorioPomPom(String name, int level){
+    public OricorioPomPom(String name, int level) {
         super(name, level);
-        setStats(75.0, 70.0, 70.0, 98.0, 70.0, 93.0);
-        setType(Type.ELECTRIC, Type.FLYING);
-        addMove(new Rest());
-        addMove(new ThunderWave());
-        addMove(new Pound());
-        addMove(new Swagger());
+        this.setStats(75.0D, 70.0D, 70.0D, 98.0D, 70.0D, 93.0D);
+        this.setType(new Type[]{Type.ELECTRIC, Type.FLYING});
+        this.addMove(new Rest());
+        this.addMove(new ThunderWave());
+        this.addMove(new Pound());
+        this.addMove(new Swagger());
     }
 }
