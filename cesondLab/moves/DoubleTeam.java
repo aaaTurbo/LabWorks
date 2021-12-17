455417670360ff623d9030c862ff4734e892465b
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:07:04 +0300

    Update DoubleTeam.java

--- cesondLab/moves/DoubleTeam.java
+++ cesondLab/moves/DoubleTeam.java
@@ -1,23 +1,25 @@
 package moves;
 
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
+import ru.ifmo.se.pokemon.StatusMove;
+import ru.ifmo.se.pokemon.Type;
 
 public class DoubleTeam extends StatusMove {
-
-    public DoubleTeam(){
-        super(Type.NORMAL, 0.0, 100.0);
+    public DoubleTeam() {
+        super(Type.NORMAL, 0.0D, 100.0D);
     }
 
-    @Override
     protected void applySelfEffects(Pokemon p) {
-        p.setMod(Stat.EVASION, +1);
-        if((int) p.getStat(Stat.ACCURACY) > 1) {
+        p.setMod(Stat.EVASION, 1);
+        if ((int)p.getStat(Stat.ACCURACY) > 1) {
             p.setMod(Stat.ACCURACY, -1);
         }
+
     }
 
-    @Override
-    protected String describe(){
+    protected String describe() {
         return "кастует Double Team";
     }
+}
 }
