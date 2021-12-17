b59d0248c0803fc0d702266ba96fea96cf6b68ee
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2021-12-17 19:09:11 +0300

    Update Rest.java

--- cesondLab/moves/Rest.java
+++ cesondLab/moves/Rest.java
@@ -1,13 +1,18 @@
 package moves;
 
-import ru.ifmo.se.pokemon.*;
+import ru.ifmo.se.pokemon.Effect;
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.Stat;
+import ru.ifmo.se.pokemon.StatusMove;
+import ru.ifmo.se.pokemon.Type;
 
 public class Rest extends StatusMove {
-    public Rest(){super(Type.PSYCHIC, 0.0, 100.00);}
+    public Rest() {
+        super(Type.PSYCHIC, 0.0D, 100.0D);
+    }
 
-    @Override
     protected void applySelfEffects(Pokemon p) {
         Effect.sleep(p);
-        p.setMod(Stat.HP, (int) (75-p.getHP()));
+        p.setMod(Stat.HP, (int)(75.0D - p.getHP()));
     }
 }
