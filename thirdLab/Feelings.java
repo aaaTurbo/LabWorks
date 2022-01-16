2677cf2fe0c725644e4c52ceb3e25c4501dc3f73
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-01-16 19:55:43 +0300

    Create Feelings.java

---
+++ thirdLab/Feelings.java
@@ -0,0 +1,18 @@
+public enum Feelings {
+
+    GREEDY("жадность"),
+    ENVY("зависть"),
+    DISGUST("отвращение"),
+    PLEASURE("удовольствие");
+
+    private String feeling;
+
+    private Feelings(String feeling){
+        this.feeling = feeling;
+    }
+
+    @Override
+    public String toString(){
+        return "Чувство " + feeling;
+    }
+}
