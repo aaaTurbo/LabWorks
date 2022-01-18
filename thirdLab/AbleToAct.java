62252dc8311b9b407e704d50870a03f1f2fc7ea0
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-01-18 12:56:40 +0300

    Delete thirdLab directory

--- thirdLab/AbleToAct.java
+++
@@ -1,4 +0,0 @@
-public interface AbleToAct {
-    public String doSimpleAction(Moves move);
-    public String doActionWithSmb(Moves move, Hero smb);
-}
--- thirdLab/HaveFeelings.java
+++
@@ -1,4 +0,0 @@
-public interface HaveFeelings {
-    public String addFeeling(Feelings feeling);
-    public String dellFeeling(Feelings feeling);
-}
