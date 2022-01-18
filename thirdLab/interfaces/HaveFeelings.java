7b370cc63edc64a94f083111fb56a285858f4977
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-01-18 13:04:37 +0300

    Create HaveFeelings.java

---
+++ thirdLab/interfaces/HaveFeelings.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+import classes.Feelings;
+
+public interface HaveFeelings {
+    public String addFeeling(Feelings feeling);
+
+    public String dellFeeling(Feelings feeling);
+}
