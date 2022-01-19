2199fc1897924b0327a62aca7ba7fc78cdb5c3d2
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-01-20 00:42:53 +0300

    Update Human.java

--- thirdLab/classes/Human.java
+++ thirdLab/classes/Human.java
@@ -1,10 +1,16 @@
 package classes;
 
 import interfaces.HaveFeelings;
+import interfaces.IsItem;
 
 public abstract class Human implements HaveFeelings {
     private String name;
     final protected int dnacode;
+    protected Feelings[] feelings = new Feelings[5];
+    public Object[] items = new Object[100];
+    public int numberOfItems = 0;
+    protected Location location;
+    protected IsItem inHand;
 
     public Human(String name, int dnacode) {
         this.dnacode = dnacode;
@@ -15,6 +21,9 @@ public abstract class Human implements HaveFeelings {
         return name;
     }
 
-    abstract public String doAction(Move move, int repeatTimes);
-
+    abstract public void takeItem(IsItem item);
+    abstract public void putItem(IsItem item);
+    abstract public void changeLocation(String newLocation);
+    abstract public void changeCity(String newLocation, String newCity);
+    abstract public void setLocation(Location location);
 }
