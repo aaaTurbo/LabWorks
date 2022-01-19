a64333d6cc95b3c8dbcfb94e7bb4e1ff56d50f77
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-01-20 00:44:09 +0300

    Update Move.java

--- thirdLab/classes/Move.java
+++ thirdLab/classes/Move.java
@@ -1,31 +1,51 @@
 package classes;
 
-public class Move {
-    private String move;
+import interfaces.IsItem;
 
-    public Move(String name) {
-        move = name;
+public class Money implements IsItem {
+    private String money;
+    private String type;
+
+    public Money(String money, String type){
+        this.money = money;
+        this.type = type;
     }
 
     @Override
-    public String toString() {
-        return " Действие " + move;
+    public String toString(){
+        return type + " " + money;
     }
 
     @Override
-    public int hashCode() {
-        return move.hashCode();
+    public int hashCode(){
+        return money.hashCode();
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
+    public boolean equals(Object o){
+        if (this == o){
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Move m = (Move) o;
-        return this.move == m.move;
+        Money m = (Money) o;
+        return this.type == m.type;
+    }
+
+    @Override
+    public void addThisItem(Hero hero) {
+        hero.items[hero.numberOfItems] = this;
+        hero.numberOfItems++;
+    }
+
+    @Override
+    public void removeThisItem(Hero hero) {
+        for(int i=0; i < hero.numberOfItems; i++){
+            if (this.equals(hero.items[i])){
+                hero.items[i] = null;
+                break;
+            }
+        }
     }
 }
