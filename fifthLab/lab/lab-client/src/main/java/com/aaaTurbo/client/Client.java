053b6bf5dd9077f0e259f7a44c85ca7189dd2f83
Anton Semenov <90704180+aaaTurbo@users.noreply.github.com>
2022-04-11 20:30:29 +0300

    Update Client.java

--- fifthLab/lab/lab-client/src/main/java/com/aaaTurbo/client/Client.java
+++ fifthLab/lab/lab-client/src/main/java/com/aaaTurbo/client/Client.java
@@ -22,7 +22,7 @@ public final class Client {
         RouteCollection mainCollection = new RouteCollection();
         CommandListener commandListener = new CommandListener(mainCollection);
         System.out.println("--- Клиент успешно настроен ---\n--- Запуск ---");
-        System.out.println("---Хотите ли вы восстановить последнюю сохрвненную коллекцию?\nВведите: да/нет");
+        System.out.println("---Хотите ли вы восстановить последнюю сохрвненную коллекцию?\nВведите: да/нет---");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String read = reader.readLine();
         if (Objects.equals("да", read) || Objects.equals("Да", read)){
